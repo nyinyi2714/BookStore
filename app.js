7f94466fb54fe3d5718160e4353ae8827c7ef580
unknown <Student@IT-STULOAN-1550.csun.edu>
2022-12-28 11:33:56 -0800

    Change cors origin

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ async function connectDB() {
 
 
 var cors = require('cors');
-app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(cors({origin: 'https://bookstore.cyclic.app', credentials: true}))
 
 const fs = require("fs")
 
